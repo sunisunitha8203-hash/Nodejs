95ddd65dfb84a50c1e58e55e758b600acd741652
Purra Sunitha <sunisunitha8203@gmail.com>
2026-06-20 19:19:21 +0530

    done

---
+++ Todolist.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+const TodoSchema=mongoose.Schema({
+    title: {
+        type:String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+module.exports=mongoose.model('todo',TodoSchema)
